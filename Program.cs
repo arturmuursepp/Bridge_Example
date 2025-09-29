b0b9da1d1769b7cbbdd2e9873be8abfbcb0c366c
arturmuursepp <artur.ateam@gmail.com>
2025-09-29 08:28:27 +0300

    Done

--- Program.cs
+++ Program.cs
@@ -1,12 +1,80 @@
-﻿namespace Bridge_Example
-{
-    public class Shape
-    {
+﻿using System;
 
+namespace Bridge_Example
+{
+    interface IColor
+    {
+        string ApplyColor();
     }
 
-    public class Color
+    class Red : IColor
     {
-        
+        public string ApplyColor() => "Red";
+    }
+    class Blue : IColor
+    {
+        public string ApplyColor() => "Blue";
+    }
+    class Green : IColor
+    {
+        public string ApplyColor() => "Green";
+    }
+
+    abstract class Shape
+    {
+        protected IColor color;
+
+        protected Shape(IColor color)
+        {
+            this.color = color;
+        }
+
+        public abstract void Draw();
+    }
+
+    class Circle : Shape
+    {
+        public Circle(IColor color) : base(color) { }
+
+        public override void Draw()
+        {
+            Console.WriteLine($"Drawing a {color.ApplyColor()} Circle");
+        }
+    }
+
+    class Square : Shape
+    {
+        public Square(IColor color) : base(color) { }
+
+        public override void Draw()
+        {
+            Console.WriteLine($"Drawing a {color.ApplyColor()} Square");
+        }
+    }
+
+    class Triangle : Shape
+    {
+        public Triangle(IColor color) : base(color) { }
+
+        public override void Draw()
+        {
+            Console.WriteLine($"Drawing a {color.ApplyColor()} Triangle");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape redCircle = new Circle(new Red());
+            Shape blueSquare = new Square(new Blue());
+            Shape greenTriangle = new Triangle(new Green());
+
+            redCircle.Draw();
+            blueSquare.Draw();
+            greenTriangle.Draw();
+
+            Console.ReadLine();
+        }
     }
 }
